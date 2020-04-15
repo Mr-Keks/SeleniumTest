7b124cade9038b76d12455c7b87942ece0224bad
Mr Keks <38166410+fuckingcoolwebsite@users.noreply.github.com>
2020-04-15 15:06:06 +0300

    V 2.2

--- SearchTest/SearchTest/Program.cs
+++ SearchTest/SearchTest/Program.cs
@@ -25,23 +25,36 @@ namespace SearchTest
         }
         [OneTimeTearDown]
         public void OneTimeTearDown() => driver.Quit();
-        [Test]
-        public void Test()
+        [TestCase(true, "Faded")]
+        [TestCase(false, "F")]
+        public void SearchTest(bool isPositive, string value)
         {
             driver.FindElement(By.Id("search_query_top")).Click();
             driver.FindElement(By.Id("search_query_top")).Clear();
-            driver.FindElement(By.Id("search_query_top")).SendKeys("F");
+            driver.FindElement(By.Id("search_query_top")).SendKeys(value);
             driver.FindElement(By.Id("search_query_top")).SendKeys(Keys.Tab);
             driver.FindElement(By.Name("submit_search")).Click();
             
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.Zero;
             wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(250));
             wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
-            bool isEmailOk = wait.Until(x =>
-                x.FindElements(By.XPath
-                    ("//div[@class='content_sortPagiBar']//div[@class='product-count']")).Any());
+            bool isSearchOk = false;
+            try
+            {
+                isSearchOk =   wait.Until(x =>
+                    x.FindElements(By.XPath
+                        ("//div[@class='content_sortPagiBar']//div[@class='product-count']")).Any());
+            }
+            catch(WebDriverTimeoutException ex)
+            {
+                Console.WriteLine($"your exeption: {ex}");
+            }
+
+          
             driver.Manage().Timeouts().ImplicitWait = implicitWait;
-            Assert.That(isEmailOk, "Email is not ok");
+            Assert.That(isSearchOk, Is.EqualTo(isPositive), $"Search was validated {(isPositive ? "successfully":"unsucessfylly")}"
+                                                            + "but were expected opposite");
+            
                 
         }
     }
