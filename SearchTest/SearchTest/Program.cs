1be3a08ec1889bef093aa30f7b2c142d1d0e585e
Mr Keks <38166410+fuckingcoolwebsite@users.noreply.github.com>
2020-04-15 22:47:04 +0300

    V 2.5

--- SearchTest/SearchTest/Program.cs
+++ SearchTest/SearchTest/Program.cs
@@ -10,35 +10,20 @@ using SearchTest.Fraemworks;
 
 namespace SearchTest
 {
-    [TestFixture]
-    public class BaseTest
+   
+    public abstract class BaseTest
     {
-        private readonly IWebDriver driver;
-        private static readonly string url = TestContext.Parameters["Url"] ?? "http://automationpractice.com/index.php";
-        private static readonly TimeSpan implicitWait = TimeSpan.FromMilliseconds(Convert.ToInt32(TestContext.Parameters["Wait"] ?? "3000"));
-        private WebDriverWait wait;
-        private MainPage mainPage;
-
-        public BaseTest()
+        protected readonly IWebDriver driver;
+        
+        protected BaseTest()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = implicitWait;
+            driver.Manage().Timeouts().ImplicitWait = Settings.ImplicitWait;
             driver.Navigate().GoToUrl(Settings.url);
-            mainPage = new MainPage(driver);
         }
         [OneTimeTearDown]
         public void OneTimeTearDown() => driver.Quit();
-        [TestCase(true, "Faded")]
-        [TestCase(false, "F")]
-        public void SearchTest(bool isPositive, string value)
-        {
-            mainPage.SearchValue(value);
-            SearchPage sp = mainPage.SearchSmth();
-            bool isSearchOk = sp.IsNewSearchElementOk();
-            Assert.That(isSearchOk, Is.EqualTo(isPositive), $"Search was validated{(isSearchOk ? "successfully": "unsuccessfully")}"
-                                                            + "but we expetcted opposite");
-
-        }
+        
     }
 }
---
+++ SearchTest/SearchTest/SearchValidationTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SearchTest.Fraemworks;
+using SearchTest.PageObjects;
+
+namespace SearchTest
+{
+    public class SearchValidationTest: BaseTest
+    {
+        private MainPage mainPage;
+        
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        { 
+            mainPage = new MainPage(driver);
+        }
+    
+        [TestCase(true, "Faded")]
+        [TestCase(false, "F")]
+        public void SearchTest(bool isPositive, string value)
+        {
+            mainPage.SearchValue(value);
+            SearchPage sp = mainPage.SearchSmth();
+            bool isSearchOk = sp.IsNewSearchElementOk();
+            Assert.That(isSearchOk, Is.EqualTo(isPositive), $"Search was validated{(isSearchOk ? "successfully": "unsuccessfully")}"
+                                                            + "but we expetcted opposite");
+
+        }
+    }
+}
